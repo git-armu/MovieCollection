e2612094ad9c7a960453cecf15d2f67b6a1fc645
git-armu <armu0001@stud.kea.dk>
2024-02-23 00:38:04 +0100

    Added menu and  loop to comply with User story #2 "As a movie enthusiast, I want to see a menu on the screen so that I can choose between 1) creation of a new movie in my movie collection and 2) exit from the program. After having created a movie, the menu must show again so I can choose either option."

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,39 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        MovieCollection movieCollection = new MovieCollection();
 
-        System.out.println("##############################");
-        System.out.println("# Welcome to your collection #");
-        System.out.println("# Press 1 to add a movie.    #");
-        System.out.println("# Press 2 to cancel.         #");
-        System.out.println("##############################");
-        int menuOption = scanner.nextInt();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            MovieCollection movieCollection = new MovieCollection();
 
-        scanner.nextLine();
+            System.out.println("##############################");
+            System.out.println("# Welcome to your collection #");
+            System.out.println("# Press 1 to add a movie.    #");
+            System.out.println("# Press 2 to cancel.         #");
+            System.out.println("##############################");
+            int menuOption = scanner.nextInt();
 
-        if (menuOption != 1) {
-            System.out.println("You have chosen to cancel. Goodbye!");
-        } else {
-            System.out.println("Movie name?");
-            String movieName = scanner.nextLine();
+            scanner.nextLine();
 
-            System.out.println("Director's name?");
-            String movieDirector = scanner.nextLine();
+            if (menuOption != 1) {
+                System.out.println("You have chosen to cancel. Goodbye!");
+                break;
 
-            System.out.println("Genre?");
-            String movieGenre = scanner.nextLine();
+            } else {
+                System.out.println("Movie name?");
+                String movieName = scanner.nextLine();
 
-            System.out.println("Your movie " + movieName + " has been added to your collection.");
-            movieCollection.addMovie(movieName, movieDirector, movieGenre);
+                System.out.println("Director's name?");
+                String movieDirector = scanner.nextLine();
+
+                System.out.println("Genre?");
+                String movieGenre = scanner.nextLine();
+
+                System.out.println("Your movie " + movieName + " has been added to your collection.");
+                movieCollection.addMovie(movieName, movieDirector, movieGenre);
+            }
+
+
         }
     }
 }
